fe3d36abeb2fab2bbc8d748b63e399fe1c921a23
root <root@LAPTOP-O4U604BL.localdomain>
2022-03-21 16:44:27 +0100

    VianneyEXO3

--- book.py
+++ book.py
@@ -7,10 +7,19 @@ class Book:
     def insert_buy(self,quantite,price):
         orderbuy = BuyOrder(quantite,price)
         self.order.append(orderbuy)
-
+        print(f"--- Insert BUY {quantite}@{price} id={len(self.order)} on {self.name}")
+        print(f"Book on {self.name}")
+        for i in range(len(self.order)):
+            print(f"         {self.order[i].type} {self.order[i].quantite}@{self.order[i].price} id={i}")
+        print(f"------------------------")
     def insert_sell(self,quantite,price):
         ordersell = SellOrder(quantite,price)
         self.order.append(ordersell)
+        print(f"--- Insert SELL {quantite}@{price} id={len(self.order)} on {self.name}")
+        print(f"Book on {self.name}")
+        for i in range(len(self.order)):
+            print(f"         {self.order[i].type} {self.order[i].quantite}@{self.order[i].price} id={i}")
+        print(f"------------------------")
 class Order : 
 
     def __init__(self, quantite, price):
@@ -18,9 +27,11 @@ class Order :
         self.price = price
 
 class BuyOrder(Order) :
-    def __init__(self):
-        super().__init__(self)
+    def __init__(self,type):
+        self.type = "BUY"
+        super().__init__(self,type)
 
 class SellOrder(Order) :
-    def __init__(self,quantite,price):
+    def __init__(self,type):
+        self.type = "SELL"
         super().__init__(self)
