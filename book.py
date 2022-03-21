a016967d2593f033a1d1c292eeea2052fc974aba
Marius Girouard <mariusgirouard@MacBook-Pro-de-Marius.local>
2022-03-21 17:52:15 +0100

    cleancode

--- book.py
+++ book.py
@@ -8,33 +8,42 @@ class Book:
     def insert_buy(self, quantite, price):
         orderbuy = BuyOrder(quantite=quantite, price=price)
         self.order.append(orderbuy)
-        print(f"--- Insert BUY {quantite}@{price} id={len(self.order)} on {self.name}")
+        print(f"--- Insert {orderbuy} id={len(self.order)} on {self.name}")
         print(f"Book on {self.name}")
         for i in range(len(self.order)):
-            print(f"         {self.order[i].type} {self.order[i].quantite}@{self.order[i].price} id={i}")
+            print(f"         {self.order[i]} id={i}")
         print(f"------------------------")
 
     def insert_sell(self, quantite, price):
         ordersell = SellOrder(quantite=quantite, price=price)
         self.order.append(ordersell)
-        print(f"--- Insert SELL {quantite}@{price} id={len(self.order)} on {self.name}")
+        print(f"--- Insert {ordersell} id={len(self.order)} on {self.name}")
         print(f"Book on {self.name}")
         for i in range(len(self.order)):
-            print(f"         {self.order[i].type} {self.order[i].quantite}@{self.order[i].price} id={i}")
+            print(f"         {self.order[i]} id={i}")
         print(f"------------------------")
 
 class Order(metaclass=abc.ABCMeta): 
     def __init__(self, quantite, price):
         self.quantite = quantite
         self.price = price
+    
+    def __str__(self) -> str:
+        return f"{self.quantite}@{self.price}"
 
 class BuyOrder(Order) :
     def __init__(self,quantite,price):
         self.type = "BUY"
         super().__init__(quantite,price)
+    
+    def __str__(self) -> str:
+        return f"{self.type} {super().__str__()}"
+
 class SellOrder(Order) :
     def __init__(self, quantite, price):
         self.type = "SELL"
         super().__init__(quantite,price)
         
+    def __str__(self) -> str:
+        return f"{self.type} {super().__str__()}"
  
